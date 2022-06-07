c8db2a28a94a50617b40b4fe75b5348610e64a4b
Kanchi Tank <kanchi.tank@gmail.com>
2022-06-08 02:44:50 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,95 @@
+"""
+ serializeImageData:  Lambda function to serialize the image data
+"""
+import json
+import boto3
+import base64
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    """A function to serialize target data from S3"""
+    
+    # Get the s3 address from the Step Function event input
+    key = event["s3_key"]
+    bucket = event["s3_bucket"]
+    
+    # Download the data from s3 to /tmp/image.png
+    boto3.resource('s3').Bucket(bucket).download_file(key, "/tmp/image.png")
+    
+    # We read the data from a file
+    with open("/tmp/image.png", "rb") as f:
+        image_data = base64.b64encode(f.read())
+
+    # Pass the data back to the Step Function
+    print("Event:", event.keys())
+    return {
+        'statusCode': 200,
+        'body': {
+            "image_data": image_data,
+            "s3_bucket": bucket,
+            "s3_key": key,
+            "inferences": []
+        }
+    }
+
+
+"""
+ImageClassifier : Lambda function to predict image classification
+"""
+import os
+import io
+import boto3
+import json
+import base64
+
+# setting the  environment variables
+ENDPOINT_NAME = 'image-classification-2021-12-04-07-33-27-658'
+# We will be using the AWS's lightweight runtime solution to invoke an endpoint.
+runtime= boto3.client('runtime.sagemaker')
+
+def lambda_handler(event, context):
+    
+    # Decode the image data
+    image = base64.b64decode(event["image_data"])
+    
+    # Make a prediction:
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                       ContentType='image/png',
+                                       Body=image)
+    
+    # We return the data back to the Step Function    
+    event["inferences"] = json.loads(response['Body'].read().decode('utf-8'))
+    return {
+        'statusCode': 200,
+        'body': event
+    }
+
+
+
+"""
+InferenceConfidenceFilter : Lambda function tofiter inference results based on confidence
+"""
+import json
+
+
+THRESHOLD = .97
+
+def lambda_handler(event, context):
+    # Get the inferences from the event
+    inferences = event["inferences"]
+    
+    # Check if any values in any inferences are above THRESHOLD
+    meets_threshold = (max(inferences) > THRESHOLD)
+    
+    # If our threshold is met, pass our data back out of the
+    # Step Function, else, end the Step Function with an error
+    if meets_threshold:
+        pass
+    else:
+        raise("THRESHOLD_CONFIDENCE_NOT_MET")
+
+    return {
+        'statusCode': 200,
+        'body': event
+    }
